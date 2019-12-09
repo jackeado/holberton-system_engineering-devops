457ec9b086aa9e121b939e343f30c4b02d413531
jackeado <pabsanchez@gmail.com>
2019-12-09 21:01:05 +0000

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create File in tpm
 
-file { 'holberton',
-     path => '/tmp/holberton',
-     mode => '0744',
-     owner => 'www-data',
-     group => 'www-data',
-     content => 'I love Puppet'
+file { 'holberton':
+  path => '/tmp/holberton',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet'
 }
