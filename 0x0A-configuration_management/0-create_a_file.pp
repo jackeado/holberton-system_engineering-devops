b4f16c67b0890a883a6deb3cb8c4446733e1935c
jackeado <pabsanchez@gmail.com>
2020-03-24 00:28:13 +0000

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,8 @@
 
 file { 'holberton':
   path    => '/tmp/holberton',
-  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet'
+  mode    => '0744',
 }
