83509000ea29a6774448a873ad24bff918c80186
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-03-26 18:40:29 -0500

    fix

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # sets up a client SSH
-include sdlib
-file_line { 'ssh_config':
-	  path => '/etc/ssh/ssh_config',
-	  line => '	IndentityFile ~/.ssh/holberton',
-	  replace => true,
+include stdlib
+file_line { 'Identity File':
+ path 	  => '/etc/ssh/ssh_config',
+ line 	  => '	IndentityFile ~/.ssh/holberton',
+ replace  => true,
 }
 
 file_line { 'Turn off pass':
-	  path => '/etc/ssh/ssh_config'
-	  line => '	PasswordAuthentication no',
-	  replace => true,
+ path 	  => '/etc/ssh/ssh_config'
+ line 	  => '	PasswordAuthentication no',
+ replace  => true,
 }
