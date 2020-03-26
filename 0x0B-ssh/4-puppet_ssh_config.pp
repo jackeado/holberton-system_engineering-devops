8654d57617c76ff7073af3661649eeade9883ab9
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-03-26 17:57:58 -0500

    puppet config

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# create File in tpm
+
+file_line { 'ssh_config':
+	  path => '/etc/ssh/ssh_config',
+	  line => 'IndentityFile ~/.ssh/holberton',
+	  replace => true,
+}
+
+file_line { 'Turno off pass'
+	  path => '/etc/ssh/ssh_config'
+	  line => 'PasswordAuthentication no',
+	  replace => true,
+}
