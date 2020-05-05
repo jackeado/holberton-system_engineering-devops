92e6c52828d9f33d8343db7bfb28a537f8bd043a
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-05-04 23:26:25 -0500

    get an User id with an Api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Uses https://jsonplaceholder.typicode.com along with an employee ID to
+return information about the employee's todo list progress
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    userId = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId), verify=False).json()
+
+    task_com = []
+    for task in todo:
+        if task.get('completed') is True:
+            task_com.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(task_com), len(todo)))
+    print("\n".join("\t {}".format(task) for task in task_com))
