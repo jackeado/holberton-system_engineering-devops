55dfc8536a088d024ff200c34375a094a33ed3eb
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-05-05 00:09:24 -0500

    fix quote

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
                         format(userId), verify=False).json()
 
-    with open("{}.csv".format(userId), 'w' newline='') as file_csv:
-        csv_write = csv.write(file_csv, csv.QUOTE_ALL)
+    with open("{}.csv".format(userId), 'w', newline='') as file_csv:
+        csv_write = csv.writer(file_csv, quoting=csv.QUOTE_ALL)
         for task in todo:
-            csv_write.writerow([(userId), task.get('username'),
+            csv_write.writerow([int(userId), user.get('username'),
                                 task.get('completed'),
                                 task.get('title')])
