126d818228b00ed59f4ce1c8fed044abaa6da797
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-05-07 20:07:11 -0500

    prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
+prints the titles of the first 10 hot posts listed for a given subreddit
 """
 import requests
 
 
 def top_ten(subreddit):
-    """ """
+    """ Top ten post """
     sub = requests.get('https://www.reddit.com/r/{}/.json'.
                        format(subreddit),
                        headers={'user-agent': 'X-Modhash'}).json()
