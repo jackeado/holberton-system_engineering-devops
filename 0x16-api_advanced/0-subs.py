22e38511d56b13c83c253857d3fe84218ad99bd9
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-05-07 19:24:41 -0500

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,13 +3,14 @@
 """
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """ """
-    sub = requests.get('https://www.reddit.com/r/{}/about.json'.
-                       format(subreddit), headers={'user-agent': 'X-Modhash'})
-    data_sub = sub.json()
-    if data_sub is None:
+    """returns the number of subscribers for a given subreddit"""
+    header = {'user-agent': 'X-Modhash'}
+    r = requests.get('https://www.reddit.com/r/{}/about.json'.
+                     format(subreddit), headers=header)
+    data_sub = r.json()
+    try:
+        dataSu = data_sub['data']['subscribers']
+    except:
         return 0
-    dataSu = data_sub['data']['subscribers']
     return(dataSu)
