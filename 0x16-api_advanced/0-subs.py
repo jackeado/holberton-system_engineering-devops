5a0793b08b0a9a2d957572ff24569b15a0a2464f
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-05-07 19:06:54 -0500

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     sub = requests.get('https://www.reddit.com/r/{}/about.json'.
                        format(subreddit), headers={'user-agent': 'X-Modhash'})
     data_sub = sub.json()
-    if dataSu is None or dataSu is not str:
+    if data_sub is None:
         return 0
     dataSu = data_sub['data']['subscribers']
     return(dataSu)
